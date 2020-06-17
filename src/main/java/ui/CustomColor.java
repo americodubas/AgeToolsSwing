9f01527fb4effdd22333bbfc541149d2be126bcd
americodubas <americo.dubas@gmail.com>
2020-06-16 23:02:58 -0300

    improve color

---
+++ src/main/java/ui/CustomColor.java
@@ -0,0 +1,9 @@
+package ui;
+
+import java.awt.*;
+
+public interface CustomColor {
+    Color DARK_GREY = new Color(62, 67, 76);
+    Color DARK_BLUE = new Color(54, 88, 128);
+    Color DARK = new Color(65, 65, 65);
+}
